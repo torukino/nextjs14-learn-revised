2ba5bd8e6b637a5143baa5258e35c42d3f04bdce
torukino <toruk0131@gmail.com>
2023-12-11 03:39:50 +0900

    Add serviceAccountKey.txt to .gitignore and update firebaseConfig.ts

--- app/lib/firebaseConfig.ts
+++ app/lib/firebaseConfig.ts
@@ -7,8 +7,13 @@ let a: any
 if (!getApps().length) {
 	// console.log('＠＠＠＠＠＠＠Initializing Firebase Admin SDK')
 
-	const serviceAccount = JSON.parse(readFileSync(resolve(process.cwd(), 'serviceAccountKey.json'), 'utf8'))
+	// const serviceAccount = JSON.parse(readFileSync(resolve(process.cwd(), 'serviceAccountKey.json'), 'utf8'))
 	// console.log('serviceAccount', serviceAccount)
+	if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+		throw new Error('FIREBASE_SERVICE_ACCOUNT環境変数が設定されていません。')
+	}
+	const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+
 	a = initializeApp({
 		projectId: process.env.FIREBASE_PROJECT_ID,
 		credential: cert(serviceAccount),
