b74474e6b54a1b39936312053249218f7016132a
Toru Kinoshita <toruk0131@gmail.com>
2023-12-15 18:27:49 +0900

    reminder

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,17 @@
-import ReminderTable from "@/app/ui/customers/reminder-table";
-
+import ReminderTable from '@/app/ui/customers/reminder-table'
+import { CreateReminder } from '@/app/ui/invoices/buttons'
+import {SearchReminder} from '@/app/ui/search'
 export default function Page() {
 	return (
-	<main>
-		<ReminderTable/>
-	</main>
+		<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+			<h1 className="text-2xl ">取引明細</h1>
+			<div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+				<SearchReminder placeholder="摘要を検索..." />
+				<CreateReminder />
+			</div>
+			<div>
+				<ReminderTable />
+			</div>
+		</div>
 	)
 }
--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,7 +1,32 @@
-import { deleteBank, updateBank } from '@/app/lib/actions'
+import { deleteBank, deleteReminder, updateBank } from '@/app/lib/actions'
 import { BANK } from '@/types/bank'
+import { REMINDER } from '@/types/reminder'
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+
+
+export function UpdateReminderButton({ reminder }: { reminder: REMINDER }) {
+	const id = reminder.id
+	return (
+		<Link href={`/dashboard/reminders/${id}/edit`} className="rounded-md border p-2 hover:bg-gray-100">
+			<PencilIcon className="w-5" />
+		</Link>
+	)
+}
+
+export function DeleteReminderButton({ id }: { id: string }) {
+	const deleteReminderWithId = deleteReminder.bind(null, id)
+	return (
+		<form action={deleteReminderWithId}>
+			<button className="rounded-md border p-2 hover:bg-gray-100">
+				<span className="sr-only">Delete</span>
+				<TrashIcon className="w-5" />
+			</button>
+		</form>
+	)
+}
+
+
 
 export function UpdateBankButton({ bank }: { bank: BANK }) {
 	const id = bank.id
@@ -23,6 +48,18 @@ export function DeleteBankButton({ id }: { id: string }) {
 		</form>
 	)
 }
+
+export function CreateReminder() {
+	return (
+		<Link
+			href="/dashboard/customers/create"
+			className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+		>
+			<span className="hidden md:block">新たな取引を作成</span> <PlusIcon className="h-5 md:ml-4" />
+		</Link>
+	)
+}
+
 
 export function CreateBank() {
 	return (
