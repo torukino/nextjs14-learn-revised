bf028b05fb34f1504ca50b32e592eb70d97790fa
torukino <toruk0131@gmail.com>
2023-12-24 23:28:45 +0900

    Update image source in SideNav component

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -14,7 +14,8 @@ export default function SideNav() {
 			</Link>
 			<div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
 				<NavLinks />
-				<Image src="/nozomi/sensei.jpg" width={`${200}`} height={`${200}`} alt="nozomi memory clinic Dr.pict" priority />
+
+				<Image className="bg-gray-50" src="/nozomi/sensei.png" width={`${200}`} height={`${200}`} alt="nozomi memory clinic Dr.pict" />
 
 				<div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
 				<form
